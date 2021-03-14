8f4cebdf8b295b356c8d02bdfbdc84279cdb8f26
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-03-14 04:45:40 -0300

    Update ex53 Prime Numbers

--- python_fundamentals_video_course/08repeating_structure_for/53prime_numbers.py
+++ python_fundamentals_video_course/08repeating_structure_for/53prime_numbers.py
@@ -9,7 +9,10 @@ for c in range(1, number + 1):
   else:
     print('\033[33m', end='')
   print(f'{c}', end=' ')
-print(f'\n\033[32mThe number {number} was divisible {total} times\n')
+print(f'\n\033[32mThe number {number} was divisible {total} times.')
+if total == 2:
+  print('\033[mAnd so it is a PRIME NUMBER.')
+else:
+  print('\033[mAnd so it is NOT A PRIME NUMBER.')
 
 # Note: use the powershell or similar terminal if you want the colors on your cmd.
- 
