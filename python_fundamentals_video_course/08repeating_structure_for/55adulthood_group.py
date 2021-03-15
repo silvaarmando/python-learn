dab33b092e6f87f763ac9dfbf190cb11e4aa5e5c
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-03-15 00:05:35 -0300

    Upgrade ex55 Adulthood Group

--- python_fundamentals_video_course/08repeating_structure_for/55adulthood_group.py
+++ python_fundamentals_video_course/08repeating_structure_for/55adulthood_group.py
@@ -1,13 +1,14 @@
 from datetime import date
 
 atual = date.today().year
-
 totmaior = 0
 totmenor = 0
+
 for pessoa in range(1, 8):
   nasc = int(input(f'Em que ano a {pessoa}ª pessoa nasceu? '))
   idade = atual - nasc
   if idade >= 21:
-  
+    totmaior += 1
   else:
-  print('Essa pessosa é menor de idade')
+    totmenor += 1
+print(f'Ao todo tivemos {totmaior} pessoa maiores de idade\nE também tivemos {totmenor} pessoas menores de idade.')
