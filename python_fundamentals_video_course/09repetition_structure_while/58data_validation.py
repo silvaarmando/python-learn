2ef00a7d573e7a4c00074aebf99e2857ec67fba6
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-03-17 23:21:05 -0300

    Update ex58 Data Validation

--- python_fundamentals_video_course/09repetition_structure_while/58data_validation.py
+++ python_fundamentals_video_course/09repetition_structure_while/58data_validation.py
@@ -1,7 +1,7 @@
 # Make a program that reads the gender of a person, but only accept the values ​​'M' or 'F'. If it is wrong, ask for the typing again until it has a correct value.
 
-sex = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+sex = str(input('Enter your gender: [M/F] ')).strip().upper()[0]
 print(sex)
 while sex not in 'MmFf':
-  sex = str(input('Dados Inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
-print(f'Sexo {sex} registrado com succeso')
+  sex = str(input('Invalid data. Please enter your gender:')).strip().upper()[0]
+print(f'Gender {sex} successfully recorded')
