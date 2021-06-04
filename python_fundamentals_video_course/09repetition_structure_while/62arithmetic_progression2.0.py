3ccc2aa94026fd3d24acf7d82df001ac5542a73f
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-06-03 21:24:16 -0300

    Add Ex62 Arithmetic Progression 2.0

---
+++ python_fundamentals_video_course/09repetition_structure_while/62arithmetic_progression2.0.py
@@ -0,0 +1,11 @@
+print('Arithmetic Progression Generator with While')
+print('\_/' * 10)
+first = int(input('First term: '))
+reason = int(input('PA reason: '))
+term = first
+count = 1
+while count <= 15:
+  print(f'{term} -> ', end='')
+  term += reason
+  count += 1
+print('END')
