66bc355b047d33f1c4c66d23b3fc2076b0195791
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-06-04 00:28:52 -0300

    Add ex63 Progression Arithmetic 3.0 +

--- python_fundamentals_video_course/09repetition_structure_while/63super_arithmetic_progression3.0.py
+++ python_fundamentals_video_course/09repetition_structure_while/63super_arithmetic_progression3.0.py
@@ -1,17 +1,18 @@
 # Improve EXERCISE 62 by asking the user if they want to show some more terms. The program ends when it says it wants to show 0 terms.
 
 print('Arithmetic Progression Generator with While')
-print('_/\_' * 10)
+print('_*_' * 15)
 
 first = int(input('First term: '))
 reason = int(input('PA reason: '))
 term = first
 count = 1
 total = 0
-more = 10
+more = 15
 
 while more != 0:
-  while count <= 15:
+  total += more
+  while count <= total:
     print(f'{term} -> ', end='')
     term += reason
     count += 1
