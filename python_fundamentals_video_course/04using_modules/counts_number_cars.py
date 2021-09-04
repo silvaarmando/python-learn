b4627edb2561d06bca2d8514f466423d26d5c3db
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-09-03 23:55:32 -0300

    Add image cars_image.jpeg

--- python_fundamentals_video_course/04using_modules/counts_number_cars.py
+++ python_fundamentals_video_course/04using_modules/counts_number_cars.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cvlib as cv 
 from cvlib.object_detection import draw_bbox
 
-img = cv2.imread('cars_image.jpgeg')
+img = cv2.imread('cars_image.jpeg')
 bbox, label, conf = cvimagedetect_common_objects(img)
 output_image = draw_bbox(img, bbox, label, conf)
 plt.imshow(output_image)
