0d291e39011032dc534e079d7d8e7cd6dd102e77
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-09-04 00:47:10 -0300

    Restore Modules

--- python_fundamentals_video_course/04using_modules/counts_number_cars.py
+++ python_fundamentals_video_course/04using_modules/counts_number_cars.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import cvlib as cv 
 from cvlib.object_detection import draw_bbox
 
-img = cv2.imread('cars_image.jpeg')
+im = cv.imread('cars_image.jpeg')
 bbox, label, conf = cvimagedetect_common_objects(img)
-output_image = draw_bbox(img, bbox, label, conf)
+output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
 plt.show()
 print(f'Number of cars in the image is' + str(label.count('car')))
