f6d08042ab83e4f85cdebbc97f59d0ed8724bec6
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-10-09 23:57:37 -0300

    Added Ex - download_youtube.py

---
+++ python_fundamentals_video_course/04using_modules/download_youtube.py
@@ -0,0 +1,7 @@
+from pytube import YouTube
+
+url = str(input('YouTube URL for download: '))
+video = YouTube(url)
+stream = video.streams.get_highest_resolution()
+
+stream.download(output_path = 'C:/')
